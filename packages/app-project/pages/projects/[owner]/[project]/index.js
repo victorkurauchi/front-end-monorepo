745be51f089398a2c6b2e4ac7625b293b2619368
Jim O'Donnell <jim@zooniverse.org>
2020-09-25 10:25:37 +0100

    Tidy up getServerSideProps Remove the getProjectWorkflowsHelper. Get the workflow language from the project.

--- packages/app-project/pages/projects/[owner]/[project]/index.js
+++ packages/app-project/pages/projects/[owner]/[project]/index.js
@@ -1,13 +1,7 @@
 import { projects } from '@zooniverse/panoptes-js'
 import fetchWorkflowsHelper from '@helpers/fetchWorkflowsHelper'
-export { default } from '@screens/ProjectHomePage'
 
-async function getProjectWorkflows([project], env) {
-  const language = 'en'
-  const { active_workflows, default_workflow } = project.links
-  const workflows = await fetchWorkflowsHelper(language, active_workflows, default_workflow, env)
-  return workflows
-}
+export { default } from '@screens/ProjectHomePage'
 
 export async function getServerSideProps({ params, query, req, res }) {
   const { owner, project } = params
@@ -17,7 +11,10 @@ export async function getServerSideProps({ params, query, req, res }) {
     slug: `${owner}/${project}`
   }
   const response = await projects.get({ query: projectQuery })
-  const workflows = await getProjectWorkflows(response.body.projects, env)
+  const [projectData] = response.body.projects
+  const language = projectData.primary_language
+  const { active_workflows, default_workflow } = projectData.links
+  const workflows = await fetchWorkflowsHelper(language, active_workflows, default_workflow, env)
   const props = { workflows }
   return ({ props })
 }
